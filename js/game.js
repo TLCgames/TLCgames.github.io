25d915d6023b979cdf9c20841f136308b974ada2
Nikhil <77420432+sourcenrao@users.noreply.github.com>
2021-03-14 13:21:09 -0400

    Add tray chips.

--- js/game.js
+++ js/game.js
@@ -1,4 +1,16 @@
 boardElement = document.getElementById("board");
+trayElement = document.getElementById("tray");
+headerElement = document.getElementById("header");
+
+headerSize = headerElement.getBoundingClientRect();
+headerWidth = headerSize.width;
+headerHeight = headerSize.height;
+headerQuarterWidth = headerWidth * 0.25;
+headerQuarterHeight = headerHeight * 0.25;
+
+traySize = trayElement.getBoundingClientRect();
+trayWidth = traySize.width;
+trayHeight = traySize.height;
 
 box = boardElement.getBoundingClientRect();
 width = box.width;
@@ -12,36 +24,48 @@ xInset = width - xOffset;
 yInset = height - yOffset;
 lineHeight = height*0.2;
 lineWidth = width*0.2;
-circleRadius = width*0.05;
+circleRadius = (height+width)*0.025;
 diagonalOffset = Math.sqrt(circleRadius**2/2);
 
-boardElement.innerHTML = `<svg width="${box.width}" height=${box.height}>
+xOffsetTray = trayWidth / 18 - 0.25;
+yCenterTray = trayHeight * 0.5;
+circleRadiusTray = (trayWidth) / 18;
+trayGap = xOffsetTray + circleRadiusTray;
+
+header.innerHTML = `
+<svg width="${headerWidth}" height="${headerHeight}">
+<g id="start" onclick="startGame()">
+    <rect x="${headerQuarterWidth}" y="${headerQuarterHeight * 2}" height="${headerQuarterHeight}" width="${headerQuarterWidth * 2}" stroke="red" rx="5" stroke-width="2" fill="transparent" />
+</g>
+</svg>`;
+
+boardElement.innerHTML = `<svg width="${width}" height=${height}>
     <g id="circle00">
-        <circle cx="${xOffset}" cy="${yOffset}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xOffset}" cy="${yOffset}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle01">
-        <circle cx="${xCenter}" cy="${yOffset}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xCenter}" cy="${yOffset}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle02">
-        <circle cx="${xInset}" cy="${yOffset}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xInset}" cy="${yOffset}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle10">
-        <circle cx="${xOffset}" cy="${yCenter}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xOffset}" cy="${yCenter}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle11">
-        <circle cx="${xCenter}" cy="${yCenter}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xCenter}" cy="${yCenter}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle12">
-        <circle cx="${xInset}" cy="${yCenter}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xInset}" cy="${yCenter}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle20">
-        <circle cx="${xOffset}" cy="${yInset}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xOffset}" cy="${yInset}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle21">
-        <circle cx="${xCenter}" cy="${yInset}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xCenter}" cy="${yInset}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
         </g>
     <g id="circle22">
-        <circle cx="${xInset}" cy="${yInset}" r="${circleRadius}" fill="darkgreen" />
+        <circle cx="${xInset}" cy="${yInset}" r="${circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" fill="darkgreen" />
     </g>
     <g id="lines">
         <line x1="${xOffset}" y1="${yOffset + circleRadius}" x2="${xOffset}" y2="${yCenter - circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" />
@@ -67,3 +91,38 @@ boardElement.innerHTML = `<svg width="${box.width}" height=${box.height}>
         <line x1="${xInset}" y1="${yOffset + circleRadius}" x2="${xInset}" y2="${yCenter - circleRadius}" style="stroke:rgb(30,220,30);stroke-width:2" />
     </g>
     </svg>`;
+
+trayElement.innerHTML = `<svg width="${trayWidth}" height=${trayHeight}>
+        <g id="chip0">
+        <circle cx="${xOffsetTray}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip1">
+        <circle cx="${xOffsetTray + trayGap}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip2">
+        <circle cx="${xOffsetTray + trayGap * 2}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip3">
+        <circle cx="${xOffsetTray + trayGap * 3}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip4">
+        <circle cx="${xOffsetTray + trayGap * 4}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip5">
+        <circle cx="${xOffsetTray + trayGap * 5}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip6">
+        <circle cx="${xOffsetTray + trayGap * 6}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip7">
+        <circle cx="${xOffsetTray + trayGap * 7}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+        <g id="chip8">
+        <circle cx="${xOffsetTray + trayGap * 8}" cy="${yCenterTray}" r="${circleRadius}" style="stroke:rgb(30,120,30);stroke-width:1" fill="darkgreen" />
+        </g>
+    </svg>`;
+
+    function startGame() {
+        assignPieces();
+        
+    }
